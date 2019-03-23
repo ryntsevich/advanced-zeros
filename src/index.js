232d05f898a53faa82b24035e3916c989d95582f
Ryntsevich Oksana <ryntsevich.oksana@gmail.com>
2019-03-24 00:36:57 +0300

    feat: Implement Advanced-zeros

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+  let newbase = base;
+  let result = null;
+  for (let i = 2; i <= base; i++) {
+    if (newbase % i === 0) {
+      let countMultiplierI = 0;
+      while (newbase % i === 0) {
+        newbase = Math.floor(newbase / i);
+        countMultiplierI++;
+      }
+      let number3 = number;
+      let countMultiplierFactorial = 0;
+      while (number3 / i > 0) {
+        countMultiplierFactorial = countMultiplierFactorial + Math.floor(number3 / i);
+        number3 = Math.floor(number3 / i);
+      }
+      if (result === null || Math.floor(countMultiplierFactorial / countMultiplierI) < result) {
+        result = Math.floor(countMultiplierFactorial / countMultiplierI);
+      }
+    }
+  }
+  return result;
 }
